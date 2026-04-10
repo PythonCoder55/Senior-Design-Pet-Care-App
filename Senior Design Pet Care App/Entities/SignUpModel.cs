42af865df098c2ed2d549a95f84af1514ae32868
Brian <Brian@DESKTOP-R45G00E>
2026-04-10 13:35:42 -0400

    Basic Style Changes on Old Master (even though it says new master)

--- Senior Design Pet Care App/Entities/SignUpModel.cs
+++ Senior Design Pet Care App/Entities/SignUpModel.cs
@@ -9,7 +9,8 @@ namespace Senior_Design_Pet_Care_App.Entities
         public string Email { get; set; } = string.Empty;
 
         [Required]
-        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        [MinLength(10, ErrorMessage = "Password must be at least 10 characters")]
+        [RegularExpression(@"^(?=.*[^a-zA-Z0-9]).+$", ErrorMessage = "Password must contain at least one special character")]
         public string Password { get; set; } = string.Empty;
 
         [Required]
